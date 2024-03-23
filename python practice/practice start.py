64f465951ccbe04ab26ba82fcdf2fc3e21f640e0
Vaibhav2005v <157788772+Vaibhav2005v@users.noreply.github.com>
2024-03-23 11:56:41 +0530

    Add files via upload

---
+++ python practice/practice start.py
@@ -0,0 +1,43 @@
+'''
+print("hello")
+print("world")
+print("hello coders")
+n= "sharan"
+age= 22
+print(n)
+print(age)
+print(type(n))
+print(type(age))
+#boolian define  true and false
+age =16
+is_adult= False
+
+name= input("what is your name?  ")
+print("hello", name)
+old_age= input("enter your old age: ")
+int(old_age)
+new_age = int(old_age) +2
+print(new_age)
+
+first = input("enter first number = ")
+second = input("enter second number = ")
+sum = int(first) + int(second)
+print(sum)
+
+name = "tony stark"
+#to conert lower to upper case and vice versa
+print(name.upper())
+print(name.lower())
+
+#to find operation with index
+print(name.find('s'))
+print(name.find('S'))
+#to find in terms of boolian
+print("tony" in name)
+print("iron" in name)
+#to replace
+print(name.replace("tony stark", "ironman"))
+##here in (name.replace("1","2"))   1=string to be changed, 2=string which will replace it
+'''
+#operators
+#to study operator go to link "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmicromerits.com%2Flesson-2-operators-in-python%2F&psig=AOvVaw0qXRPLBTMLNlYsfzIophh5&ust=1711259800933000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJCmr-7ZiYUDFQAAAAAdAAAAABAD"
